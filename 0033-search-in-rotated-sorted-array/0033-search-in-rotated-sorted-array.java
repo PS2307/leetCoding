3ad0c849e3d6563ae98a0aeb9dca5f4f79c02819
Siddharth <81458908+PS2307@users.noreply.github.com>
2023-09-29 00:24:07 +0530

    Time: 0 ms (100%), Space: 40.7 MB (75.86%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,13 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length==0) return -1;
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],i);
+        // if(nums.length==0) return -1;
+        // Map<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     map.put(nums[i],i);
+        // }
+        // if(map.containsKey(target)) return map.get(target);
+        // else return -1;
+
+        return modifiedBinary(nums,target,0,nums.length-1);
+        
+        
+    }
+    private int modifiedBinary(int[] arr, int target, int left, int right){
+        if(left>right) return -1;
+        int mid=left+(right-left)/2;
+        if(arr[mid]==target) return mid;
+
+        
+        if(arr[mid]>=arr[left]){
+            if(target<=arr[mid] && target>=arr[left]) return modifiedBinary(arr,target,left,mid-1);
+            else return modifiedBinary(arr,target,mid+1,right);
         }
-        if(map.containsKey(target)) return map.get(target);
-        else return -1;
-        
-        
+        else{
+            if(target>=arr[mid] && target<=arr[right]) return modifiedBinary(arr,target,mid+1,right);
+            else return modifiedBinary(arr,target,left,mid-1);
+        }
+
     }
 }
