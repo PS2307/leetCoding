3f65f54e53101ea86676a68a84417dd4a6c104e3
Siddharth <81458908+PS2307@users.noreply.github.com>
2023-10-04 21:36:10 +0530

    Time: 0 ms (100%), Space: 39.8 MB (73.44%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -15,12 +15,32 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root==null) return root;
+        // recursive
+
+       /* if(root==null) return root;
         TreeNode left=invertTree(root.left);
         TreeNode right=invertTree(root.right);
         root.left=right;
         root.right=left;
         return root;
+        */
+
+        //iterative
+        if(root==null) return root;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode node=queue.poll();
+
+            TreeNode temp=node.left;
+            node.left=node.right;
+            node.right=temp;
+
+            if(node.left!=null) queue.add(node.left);
+            if(node.right!=null) queue.add(node.right);
+
+        }return root;
+
 
         
     }
