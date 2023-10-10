62a5e2b2108c18ef20acd2824602fb5756b3c98d
Siddharth <81458908+PS2307@users.noreply.github.com>
2023-10-10 15:40:07 +0530

    Time: 0 ms (100%), Space: 40.9 MB (89.54%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,41 +10,42 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head = new ListNode(-1);
-        ListNode cur=head;
+        // ListNode head = new ListNode(-1);
+        // ListNode cur=head;
 
-        while(list1!=null && list2!=null ){
-            if(list1.val<=list2.val){
-                cur.next=list1;
-                list1=list1.next;
-            }else{
-                cur.next=list2;
-                list2=list2.next;
-            }cur=cur.next;
-        }
-        cur.next = list1 == null ? list2 : list1;
-        return head.next;
-
-        // if(list1==null) return list2;
-        // if(list2==null) return list1;
-        // if(list1.val>list2.val){
-        //     ListNode temp=l1;
-        //     list1=list2;
-        //     list2=temp;
-        // }
-        // ListNode res=list1;
-        
-        // while(list1!=null && list2!=null){
-        //     ListNode temp=null;
+        // while(list1!=null && list2!=null ){
         //     if(list1.val<=list2.val){
-        //         temp=list1;
+        //         cur.next=list1;
         //         list1=list1.next;
-                
         //     }else{
-        //         list1.
-
-        //     }
+        //         cur.next=list2;
+        //         list2=list2.next;
+        //     }cur=cur.next;
         // }
+        // cur.next = list1 == null ? list2 : list1;
+        // return head.next;
+
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        if(list1.val>list2.val){
+            ListNode temp=list1;
+            list1=list2;
+            list2=temp;
+        }
+        ListNode res=list1;
+        
+        while(list1!=null && list2!=null){
+            ListNode temp=null;
+            while(list1!=null && list1.val<=list2.val){
+                temp=list1;
+                list1=list1.next;
+                
+            }temp.next=list2;
+
+            ListNode swap=list1;
+            list1=list2;
+            list2=swap;
+        }return res;
       
     }
 }
